0c478f57577f906fcadbccb90b435992737de944
Dave <crazeydave@users.noreply.github.com>
2026-01-14 04:26:01 +0000

    added contact support to groups and meetings, rest controller.

--- tsml-for-unity.php
+++ tsml-for-unity.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * Plugin URI: https://github.com/bleeding-deacons/tsml-for-unity
  * Description: Integrates 12 Step Meeting List (TSML) with the Unity plugin, providing meeting, group & location support.
  * Version: 1.1.0
- * Requires at least: 5.0
+ * Requires at least: 6.0
  * Requires PHP: 8
  * Author: The Bleeding Deacons
  * Author URI: thebleedingdeacons@gmail.com
