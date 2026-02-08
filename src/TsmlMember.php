7f2e10cb6bbab66181444391b132706e0fc2716a
Dave <crazeydave@users.noreply.github.com>
2026-02-08 04:40:01 +0000

    Removed private name field (was not used anyway) Audit Logging, on view, on edit, on delete.

--- src/TsmlMember.php
+++ src/TsmlMember.php
@@ -86,10 +86,10 @@ class TsmlMember implements MemberInterface
         return $this->anonymousName;
     }
 
-    public function getPrivateName(): string
-    {
-        return $this->privateName;
-    }
+//    public function getPrivateName(): string
+//    {
+//        return $this->privateName;
+//    }
 
     public function getEmail(): string
     {
